7d754947fa377f7cc53b10c78d377fff3e22b8f9
Andrey <milanastrunevskaya@gmail.com>
2022-11-04 21:32:05 +0300

    add Counter

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,15 +1,50 @@
 import { Component } from '../../core';
+import './Button.scss';
 
 export class Button extends Component {
 	constructor() {
 		super();
 		this.state = {
-			isActive: false,
+			count: 0,
 		};
 	}
 
+	addCount() {
+		this.setState((state) => {
+			return {
+				...state,
+				count: state.count + 1,
+			};
+		});
+	}
+
+	minusCount() {
+		this.setState((state) => {
+			return {
+				...state,
+				count: state.count - 1,
+			};
+		});
+	}
+
+	registerEvents() {
+		this.addEventListener('click', (e) => {
+			this.setState((state) => {
+				if (e.target.closest('.plus')) {
+					this.addCount();
+				} else if (e.target.closest('.minus')) {
+					this.minusCount();
+				}
+			});
+		});
+	}
+
 	render() {
-		return `<button>Click</button>`;
+		return `
+			<button data-action="plus">+</button>
+			<span>${this.state.count}</span>
+			<button data-action="minus">-</button>
+		`;
 	}
 }
 
