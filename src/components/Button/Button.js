d471ae456e7e98fa8f4cfcac7b410d36770c54a1
Andrey <milanastrunevskaya@gmail.com>
2022-11-04 20:29:46 +0300

    add Button

---
+++ src/components/Button/Button.js
@@ -0,0 +1,16 @@
+import { Component } from '../../core';
+
+export class Button extends Component {
+	constructor() {
+		super();
+		this.state = {
+			isActive: false,
+		};
+	}
+
+	render() {
+		return `<button>Click</button>`;
+	}
+}
+
+customElements.define('my-button', Button);
